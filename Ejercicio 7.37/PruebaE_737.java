dd53967132ff973761b61ddce8314908f511e5e3
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-06-26 20:06:12 -0600

    Add files via upload

---
+++ Ejercicio 7.37/PruebaE_737.java
@@ -0,0 +1,20 @@
+
+
+public class PruebaE_737 {
+    public static void main( String args[] )
+    {
+        E_737 simp = new E_737();
+        int operands=0;
+        int operation=0;
+
+        simp.displayWelcomeMessage();
+        simp.runSimulator();
+        simp.initialiseVariables();
+        simp.load();
+        simp.execute(operands,operation);
+        simp.dumpTheCore();
+
+
+    }
+
+}
